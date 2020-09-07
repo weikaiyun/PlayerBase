a52b631dccb74be5c0511481af5d30fc4f9ba10d
weikaiyun <weikaiyun@pwrd.com>
2020-09-07 15:50:25 +0800

    OrientationSensor优化

--- app/src/main/java/com/kk/taurus/avplayer/utils/OrientationSensor.java
+++ app/src/main/java/com/kk/taurus/avplayer/utils/OrientationSensor.java
@@ -40,10 +40,6 @@ public class OrientationSensor {
     private OrientationSensorListener listener;
     private Sensor sensor;
 
-    private SensorManager sm1;
-    private Sensor sensor1;
-    private OrientationSensorListener1 listener1;
-
     private Handler mHandler = new Handler(Looper.getMainLooper()) {
         public void handleMessage(Message msg) {
             switch (msg.what) {
@@ -93,11 +89,6 @@ public class OrientationSensor {
         sm = (SensorManager) activity.getSystemService(Context.SENSOR_SERVICE);
         sensor = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         listener = new OrientationSensorListener(mHandler);
-
-        // 根据 旋转之后/点击全屏之后 两者方向一致,激活sm.
-        sm1 = (SensorManager) activity.getSystemService(Context.SENSOR_SERVICE);
-        sensor1 = sm1.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        listener1 = new OrientationSensorListener1();
     }
 
     private void callbackOnLandScape(int orientation){
@@ -110,30 +101,12 @@ public class OrientationSensor {
             onOrientationListener.onPortrait(orientation);
     }
 
-    /**
-     * 手动横竖屏切换方向
-     */
-    public void toggleScreen() {
-        sm.unregisterListener(listener);
-        sm1.registerListener(listener1, sensor1,SensorManager.SENSOR_DELAY_UI);
-        if (isPortrait) {
-            isPortrait = false;
-            // 切换成横屏
-            callbackOnLandScape(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        } else {
-            isPortrait = true;
-            // 切换成竖屏
-            callbackOnPortrait(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        }
-    }
-
     public void enable(){
         sm.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_UI);
     }
 
     public void disable(){
         sm.unregisterListener(listener);
-        sm1.unregisterListener(listener1);
     }
 
     public boolean isPortrait(){
@@ -183,55 +156,6 @@ public class OrientationSensor {
             }
             if (rotateHandler != null) {
                 rotateHandler.obtainMessage(MSG_SENSOR, orientation, 0).sendToTarget();
-            }
-        }
-    }
-
-    public class OrientationSensorListener1 implements SensorEventListener {
-        private static final int _DATA_X = 0;
-        private static final int _DATA_Y = 1;
-        private static final int _DATA_Z = 2;
-
-        public static final int ORIENTATION_UNKNOWN = -1;
-
-        public OrientationSensorListener1() {
-        }
-
-        public void onAccuracyChanged(Sensor arg0, int arg1) {
-        }
-
-        public void onSensorChanged(SensorEvent event) {
-            float[] values = event.values;
-            int orientation = ORIENTATION_UNKNOWN;
-            float X = -values[_DATA_X];
-            float Y = -values[_DATA_Y];
-            float Z = -values[_DATA_Z];
-            float magnitude = X * X + Y * Y;
-            // Don't trust the angle if the magnitude is small compared to the y
-            // value
-            if (magnitude * 4 >= Z * Z) {
-                // 屏幕旋转时
-                float OneEightyOverPi = 57.29577957855f;
-                float angle = (float) Math.atan2(-Y, X) * OneEightyOverPi;
-                orientation = 90 - (int) Math.round(angle);
-                // normalize to 0 - 359 range
-                while (orientation >= 360) {
-                    orientation -= 360;
-                }
-                while (orientation < 0) {
-                    orientation += 360;
-                }
-            }
-            if (orientation > 225 && orientation < 315) {// 检测到当前实际是横屏
-                if (!isPortrait) {
-                    sm.registerListener(listener, sensor,SensorManager.SENSOR_DELAY_UI);
-                    sm1.unregisterListener(listener1);
-                }
-            } else if ((orientation > 315 && orientation < 360) || (orientation > 0 && orientation < 45)) {// 检测到当前实际是竖屏
-                if (isPortrait) {
-                    sm.registerListener(listener, sensor,SensorManager.SENSOR_DELAY_UI);
-                    sm1.unregisterListener(listener1);
-                }
             }
         }
     }
